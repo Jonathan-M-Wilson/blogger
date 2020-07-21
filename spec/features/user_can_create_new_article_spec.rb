d009ca4676621c55cf3c1afee7067d3a5163fe09
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-21 08:03:24 -0600

    :white_check_mark: Add a spec for creating a new article

---
+++ spec/features/user_can_create_new_article_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "User can create a new article", type: :feature do
+  describe "When they link from the articles index" do
+    describe "And they fill in a title and body" do
+      it "allows a user to click on a link to create a new article" do
+        
+        visit articles_path
+        click_link "Create a New Article"
+
+        expect(current_path).to eq(new_article_path)
+
+        fill_in "article[title]", with: "New Sample Title!"
+        fill_in "article[body]",  with: "New Sample Body!"
+        click_on "Create Article"
+
+        expect(page).to have_content("New Sample Title!")
+        expect(page).to have_content("New Sample Body!")
+      end
+    end
+  end
+end
