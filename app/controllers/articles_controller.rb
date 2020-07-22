4074543f027e03c389ff1cc3dca26f79eef73c4b
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-07-22 08:51:01 -0600

    :zap: Add flash notices to create/update/destroy methods in ArticlesController

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,16 +15,19 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+    flash.notice = "Article '#{@article.title}' has been created!"
     redirect_to article_path(@article)
   end
 
   def update
     @article.update(article_params)
+    flash.notice = "Article '#{@article.title}' has been updated!"
     redirect_to article_path(@article)
   end
 
   def destroy
     @article.destroy
+    flash.notice = "Article '#{@article.title}' has been deleted!"
     redirect_to articles_path
   end
 
