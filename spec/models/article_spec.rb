fe73fe0e80c2188afcea22c7eb0b6316b298953a
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-11-19 16:30:30 -0700

    :zap: Update relationships in article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -6,5 +6,6 @@ describe Article, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:title)}
     it {should validate_presence_of(:body)}
+    it {should have_many(:comments)}
   end
 end
