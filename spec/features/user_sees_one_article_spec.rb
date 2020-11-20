172f0911231aef52744ae67e91466a8ae3e47329
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-11-19 17:08:49 -0700

    :zap: Correct comment creation mistake in before each block in spec

--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe 'As a visitor', type: :feature do
   before(:each) do
     @article_1 = Article.create!(title: 'Title 1', body: 'Body 1')
     @article_2 = Article.create!(title: 'Title 2', body: 'Body 2')
-    @comment_1 = article.comments.create(author_name: "John Wayne", body: "Courage is being scared to death but saddling up anyway.")
-    @comment_2 = article.comments.create(author_name: "Kathy", body: "Wow! I love this so much!")
+    @comment_1 = @article_1.comments.create(author_name: "John Wayne", body: "Courage is being scared to death but saddling up anyway.")
+    @comment_2 = @article_1.comments.create(author_name: "Kathy", body: "Wow! I love this so much!")
   end
 
   describe 'user can see one article' do
